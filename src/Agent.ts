7619435773222b398566ef955f0fd5f94326dd7b
yujuiting <yujuiting@me.com>
2020-12-02 14:22:57 +0800

    fix matomo

--- src/Agent.ts
+++ src/Agent.ts
@@ -121,12 +121,15 @@ export interface MatomoAgentConfig {
 }
 
 export class MatomoAgent extends Agent {
-  private client = window._paq || []; // eslint-disable-line no-underscore-dangle
+  private client = window._paq || [];
 
   private trackPageViewTimer = 0;
 
   constructor(readonly config: MatomoAgentConfig) {
     super();
+
+    window._paq = window._paq || []; // eslint-disable-line no-underscore-dangle
+    this.client = window._paq; // eslint-disable-line no-underscore-dangle
   }
 
   async doInitialize() {
