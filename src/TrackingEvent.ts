824ed69bce07d6fe2db010ae291e220b7c3385dd
DolphinLin <hisb50918@gmail.com>
2020-12-11 15:09:38 +0800

    update

--- src/TrackingEvent.ts
+++ src/TrackingEvent.ts
@@ -134,6 +134,17 @@ export interface SectionViewEvent extends BaseEvent<SectionViewEventPayload> {
   action: 'scroll';
 }
 
+interface SearchUserEventPayload extends BasePayload {
+  searchString: string;
+  resultCount: number;
+}
+
+export interface SearchUserEvent extends BaseEvent<SearchUserEventPayload> {
+  category: 'Content';
+  action: 'search';
+  name: 'search_user';
+}
+
 export type TrackingEvent =
   | ClickButtonEvent
   | ClickTabEvent
